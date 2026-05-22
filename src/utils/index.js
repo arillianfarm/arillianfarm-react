a69058cf01fbe2f38989ea000bcf3da1fee75e77
Arillian Farm <arillianfarm@gmail.com>
2026-05-22 09:43:33 -0700

    still working on content but need to take pajama to the vet now

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,8 @@
 
 export const titleCaps = (str) => {
     if (!str) return '';
-    return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    return str.toUpperCase();
+    //toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 };
 
 export const trunc = (str, length = 50) => {
